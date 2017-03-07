7d0bfcddb29297b08b2aa038a73f9672d6ec5fc3
Grace Montoya <potsifera@gmail.com>
2017-03-07 13:56:15 -0600

    adds changeTeeth function to change teeth # and length

--- gearCreator.py
+++ gearCreator.py
@@ -22,3 +22,18 @@ def createGear(teeth=10, length=0.3):
     extrude = cmds.polyExtrudeFacet(localTranslateZ=length)[0]
 
     return transform, constructor, extrude
+
+def changeTeeth(constructor, extrude, teeth=10, length=0.3):
+    spans = teeth * 2
+    cmds.polyPipe(constructor, edit=True,subdivisionsAxis=spans)
+
+    sideFaces = range(spans*2, spans*3,2)
+    faceNames = []
+
+    for face in sideFaces:
+        faceName = 'f[%s]' % (face)
+        faceNames.append(faceName)
+
+    cmds.setAttr('%s.inputComponents' % (extrude), len(faceNames),*faceNames,type="componentList")
+
+    cmds.polyExtrudeFacet(extrude, edit=True, ltz = length)
