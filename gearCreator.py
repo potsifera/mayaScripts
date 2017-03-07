6538542bad216e2c52c2dfc2723a21aae1c5c44f
Grace Montoya <potsifera@gmail.com>
2017-03-07 13:01:00 -0600

    function that creates a gear and gives back data

--- gearCreator.py
+++ gearCreator.py
@@ -1,1 +1,22 @@
-print "Hello,Maya!"
+#controls how many teeth a gear has
+
+
+
+from maya import cmds
+
+def createGear(teeth=10, length=0.3):
+    #teeth are every alternate face, so spans x 2
+    spans = teeth*2
+
+    transform, constructor = cmds.polyPipe(subdivisionsAxis=spans)
+    sideFaces = range(spans*2, spans*3, 2)
+
+    cmds.select(clear = True)
+
+    for face in sideFaces:
+        #pPipe1.f[41] pPipe1.f[43]  selects every other face in the pipe
+        cmds.select('%s.f[%s]' % (transform,face), add=True)
+
+    extrude = cmds.polyExtrudeFacet(localTranslateZ=length)[0]
+
+    return transform, constructor, extrude
