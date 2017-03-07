b574d0116353f74e9ae8b02091a1abf6d0f146e2
Grace Montoya <potsifera@gmail.com>
2017-03-07 16:42:06 -0600

    adds self properties to createGear and changeTeeth

--- gearClassCreator.py
+++ gearClassCreator.py
@@ -4,22 +4,21 @@ class Gear(object):
 
     def createGear(self, teeth=10, length=0.3):
         spans=teeth*2
-        transform, constructor = cmds.polyPipe(subdivisionsAxis=spans)
+        self.transform, self.constructor = cmds.polyPipe(subdivisionsAxis=spans)
         sideFaces = range(spans*2, spans*3, 2)
 
         cmds.select(clear = True)
 
         for face in sideFaces:
             #pPipe1.f[41] pPipe1.f[43]  selects every other face in the pipe
-            cmds.select('%s.f[%s]' % (transform,face), add=True)
+            cmds.select('%s.f[%s]' % (self.transform,face), add=True)
 
-        extrude = cmds.polyExtrudeFacet(localTranslateZ= length)[0]
+        self.extrude = cmds.polyExtrudeFacet(localTranslateZ= length)[0]
 
-        return transform, constructor, extrude
 
-    def changeTeeth(self,constructor, extrude, teeth=10, length=0.3):
+    def changeTeeth(self, teeth=10, length=0.3):
         spans = teeth * 2
-        cmds.polyPipe(constructor, edit=True,subdivisionsAxis=spans)
+        cmds.polyPipe(self.constructor, edit=True,subdivisionsAxis=spans)
 
         sideFaces = range(spans*2, spans*3,2)
         faceNames = []
@@ -28,7 +27,7 @@ class Gear(object):
             faceName = 'f[%s]' % (face)
             faceNames.append(faceName)
 
-        cmds.setAttr('%s.inputComponents' % (extrude), len(faceNames),*faceNames,type="componentList")
+        cmds.setAttr('%s.inputComponents' % (self.extrude), len(faceNames),*faceNames,type="componentList")
 
-        cmds.polyExtrudeFacet(extrude, edit=True, ltz = length)
+        cmds.polyExtrudeFacet(self.extrude, edit=True, ltz = length)
 
