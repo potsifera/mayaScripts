861016d03987192cb871d7557f92e126fe391b97
Grace Montoya <potsifera@gmail.com>
2017-03-09 12:42:07 -0600

    adds close button

--- tweenerUI.py
+++ tweenerUI.py
@@ -84,10 +84,16 @@ class TweenWindow(object):
         self.slider = cmds.floatSlider(min=0, max =100, value=50, step=1, changeCommand=tween)
         cmds.button(label="reset", command=self.reset)
 
+        cmds.setParent(column)
+        cmds.button(label="Close",command=self.close)
+
     def reset(self, *args):
-        print("resetting UI")
-        print args
         cmds.floatSlider(self.slider, edit=True, value=50)
+
+    def close(self, *args):
+        cmds.deleteUI(self.windowName)
+
+
 
 
 
