f5ec0cf81bd3ea5286905ceec9e81875254a5137
Grace Montoya <potsifera@gmail.com>
2017-03-09 17:25:24 -0600

    adds conLibrary

--- conLibrary/controllerLibrary.py
+++ conLibrary/controllerLibrary.py
@@ -17,3 +17,38 @@ def createDirectory(directory=DIRECTORY):
     if not os.path.exists(directory):
         os.mkdir(directory)
 
+class ControllerLibrary(dict):
+
+    def save(self, name, directory=DIRECTORY):
+
+        createDirectory(directory)
+
+        path = os.path.join(directory,'%s.ma' % name)
+
+        cmds.file(rename=path)
+
+        if cmds.ls(selection=True):
+            cmds.file(force=True, type='mayaAscii', exportSelected=True)
+        else:
+             cmds.file(save=True,type='mayaAscii',force=True)
+
+
+
+    def find(self, directory=DIRECTORY):
+        if not os.path.exists(directory):
+            return
+
+        files  = os.listdir(directory)
+        mayaFiles = [f for f in files if f.endswith('.ma')]
+
+
+        for ma in mayaFiles:
+            name, ext = os.path.splitext(ma)
+            path = os.path.join(directory, ma)
+
+            self[name] = path
+
+        pprint.pprint(self)
+
+
+
