b4aec3e88dd94db5c5a1ab348ad4705a5d810cae
Grace Montoya <potsifera@gmail.com>
2017-03-09 13:28:24 -0600

    adds class TweenerUI

--- reusableUI.py
+++ reusableUI.py
@@ -21,7 +21,26 @@ class BaseWindow(object):
 
     def reset(self, *args):
         pass
+
     def close(self, *args):
         cmds.deleteUI(self.windowName)
 
 
+class TweenerUI(BaseWindow):
+    windowName = "tweenerWindow"
+
+    def buildUI(self):
+        column = cmds.columnLayout()
+        cmds.text(label="use this slider to set the tween amount")
+
+        row = cmds.rowLayout(numberOfColumns=2)
+        self.slider = cmds.floatSlider(min=0, max =100, value=50, step=1, changeCommand=tween)
+        cmds.button(label="reset", command=self.reset)
+
+        cmds.setParent(column)
+        cmds.button(label="Close",command=self.close)
+
+    def reset(self, *args):
+        cmds.floatSlider(self.slider, edit=True, value=50)
+
+    
