a7f5147d3c3e47f1098db810ee6b75473f62dc1d
Grace Montoya <potsifera@gmail.com>
2017-03-10 12:21:04 -0600

    adds documentation

--- gearCreator.py
+++ gearCreator.py
@@ -18,29 +18,31 @@ def createGear(teeth=10, length=0.3):
     spans = teeth*2
 
     transform, constructor = cmds.polyPipe(subdivisionsAxis=spans)
-    sideFaces = range(spans*2, spans*3, 2)
+    sideFaces = range(spans*2, spans*3, 2) #select faces & then ls -sl in mel
 
+    #clears selection
     cmds.select(clear = True)
-
+    #selects only the faces we need to extrude
     for face in sideFaces:
         #pPipe1.f[41] pPipe1.f[43]  selects every other face in the pipe
         cmds.select('%s.f[%s]' % (transform,face), add=True)
 
-    extrude = cmds.polyExtrudeFacet(localTranslateZ=length)[0]
+    extrude = cmds.polyExtrudeFacet(localTranslateZ=length)[0] #selects only the name from the list
 
-    return transform, constructor, extrude
+    return transform, constructor, extrude #pPipe1 polyPipe1 polyExtrudeFace1
 
 def changeTeeth(constructor, extrude, teeth=10, length=0.3):
     spans = teeth * 2
-    cmds.polyPipe(constructor, edit=True,subdivisionsAxis=spans)
+    cmds.polyPipe(constructor, edit=True, subdivisionsAxis=spans)
 
-    sideFaces = range(spans*2, spans*3,2)
+    sideFaces = range(spans*2, spans*3, 2)
+    #gets the faceNames
     faceNames = []
-
     for face in sideFaces:
         faceName = 'f[%s]' % (face)
         faceNames.append(faceName)
 
-    cmds.setAttr('%s.inputComponents' % (extrude), len(faceNames),*faceNames,type="componentList")
-
-    cmds.polyExtrudeFacet(extrude, edit=True, ltz = length)
+    #sets
+    cmds.setAttr('%s.inputComponents' % (extrude), len(faceNames), *faceNames, type="componentList") #duda
+    #extrudes
+    cmds.polyExtrudeFacet(extrude, edit=True, ltz=length) #edit the polyExtrudeFace1
